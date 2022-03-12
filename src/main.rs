1cd6bac1e986eb1f92e98809c6bbd2e5b18e2c66
Galen Guyer <galen@galenguyer.com>
2022-03-12 17:38:03 -0500

    use local bind addr

--- src/main.rs
+++ src/main.rs
@@ -84,8 +84,8 @@ async fn main() -> Result<(), sqlx::Error> {
                 .layer(Extension(oidc_client)),
         );
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
-    info!("starting on http://{}", addr);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    info!("Listening on http://{}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
