8113bca55e3de84bbe509f52167f1ee89a8cf51e
Galen Guyer <galen@galenguyer.com>
2022-03-12 16:18:28 -0500

    add route for updating drink credits for user

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
 
 use bartender::ldap::client as ldap_client;
-use bartender::ldap::user::*;
 use bartender::oidc::{auth::OIDCAuth, client as oidc_client};
 use bartender::routes;
 
@@ -54,9 +53,11 @@ async fn main() -> Result<(), sqlx::Error> {
     let app = Router::new()
         .route("/drinks", get(routes::compat::drinks::get_drinks))
         .route("/drinks/drop", post(routes::compat::drinks::drop))
-        .route("/users/credits", get(routes::compat::users::get_credits))
+        .route(
+            "/users/credits",
+            get(routes::compat::users::get_credits).put(routes::compat::users::set_credits),
+        )
         .route("/auth_test", get(auth_test))
-        .route("/ldap_test", get(ldap_test))
         .route("/search_users", get(users_search))
         .layer(
             ServiceBuilder::new()
@@ -78,40 +79,6 @@ async fn main() -> Result<(), sqlx::Error> {
 
 async fn auth_test(OIDCAuth(user): OIDCAuth) -> impl IntoResponse {
     format!("{:#?}", user)
-}
-
-async fn ldap_test(
-    Extension(mut ldap): Extension<ldap_client::LdapClient>,
-    Query(params): Query<HashMap<String, String>>,
-) -> impl IntoResponse {
-    let uid = params.get("uid").map(|id| id.to_owned());
-    if uid.is_none() {
-        return (
-            StatusCode::BAD_REQUEST,
-            Json(json!({"error":"no uid given"})),
-        );
-    }
-
-    let user = ldap.get_user(&uid.clone().unwrap()).await;
-    let credits = params.get("credits").map(|num| num.parse::<i64>());
-
-    match (user, credits) {
-        (None, _) => (
-            StatusCode::NOT_FOUND,
-            Json(json!({"error": "user not found"})),
-        ),
-        (Some(u), None) => (StatusCode::OK, Json(json!(u))),
-        (Some(u), Some(c)) => {
-            let change_set = LdapUserChangeSet {
-                dn: u.clone().dn,
-                drinkBalance: Some(c.unwrap()),
-                ibutton: None,
-            };
-            ldap.update_user(&change_set).await;
-            let user = ldap.get_user(&uid.unwrap()).await.unwrap();
-            (StatusCode::OK, Json(json!(user)))
-        }
-    }
 }
 
 async fn users_search(
--- src/routes/compat/users.rs
+++ src/routes/compat/users.rs
@@ -1,8 +1,10 @@
 use crate::ldap::client as ldap_client;
+use crate::ldap::user::LdapUserChangeSet;
 use axum::extract::{Extension, Query};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::Json;
+use itertools::Itertools;
 use serde_json::json;
 use std::collections::HashMap;
 
@@ -65,4 +67,70 @@ pub async fn get_credits(
             ),
         )
     }
+}
+
+pub async fn set_credits(
+    Extension(mut ldap): Extension<ldap_client::LdapClient>,
+    Json(body): Json<serde_json::Value>,
+) -> impl IntoResponse {
+    let uid = body["uid"].as_str();
+    let new_balance = body["drinkBalance"].as_i64();
+
+    let mut unprovided: Vec<String> = Vec::new();
+    if uid.is_none() {
+        unprovided.push(String::from("uid"))
+    }
+    if new_balance.is_none() {
+        unprovided.push(String::from("drinkBalance"))
+    }
+
+    if !unprovided.is_empty() {
+        return (
+            StatusCode::BAD_REQUEST,
+            Json(json!({
+                "message":
+                    format!(
+                        "The following required parameters were not provided: {}",
+                        unprovided.iter().join(", ")
+                    )
+            })),
+        );
+    }
+
+    let user = ldap.get_user(uid.unwrap()).await;
+
+    if user.is_none() {
+        return (
+            StatusCode::BAD_REQUEST,
+            Json(json!({
+                "message":
+                    format!(
+                        "The requested uid '{}' does not belong to any user.",
+                        uid.unwrap()
+                    )
+            })),
+        );
+    }
+    let user = user.unwrap();
+
+    let old_credits = user.drinkBalance.unwrap_or(0);
+    let change_set = LdapUserChangeSet {
+        dn: user.clone().dn,
+        drinkBalance: Some(new_balance.unwrap()),
+        ibutton: None,
+    };
+    ldap.update_user(&change_set).await;
+    let user = ldap.get_user(uid.unwrap()).await.unwrap();
+    let new_balance = user.drinkBalance.unwrap_or(0);
+
+    (
+        StatusCode::OK,
+        Json(json!({
+            "message":
+                format!(
+                    "Drink balance updated from {} credits to {} credits for user '{}'",
+                    old_credits, new_balance, user.uid
+                )
+        })),
+    )
 }
